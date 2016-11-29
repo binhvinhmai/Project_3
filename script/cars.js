2c89cd59aa2c804b022d9a7a66cd18c611c99e68
Binh Vinh Mai <bvmvw5@umkc.edu>
2016-11-28 21:45:48 -0600

    Started search feature

--- script/cars.js
+++ script/cars.js
@@ -1,5 +1,25 @@
 $(document).ready(init);
 
-function init() {
 
+//Copied straight from search.hs in student_ajax, need to modify actually
+function init(){
+    $("#search_button").on("click",view_students);
+    $("#search").on("keydown",function(event){view_students_key(event);});
+}
+
+function view_students_key(event) {
+    if (event.keyCode == 13) //ENTER KEY
+        view_students();
+}
+
+function view_students() {
+    $.ajax({
+        method: "POST",
+        url: "search.php",
+        dataType: "text", //return text data
+        data: {search: $("#search").val()}, //send the value of the search box
+        success: function (data) {
+            $("#students").html(data); //the returned result is the HTML of the students div
+        }
+    });   
 }
---
+++ server/search.php
@@ -0,0 +1,6 @@
+<?php
+
+require_once 'connection.php';
+$student = ""; //HTML for the cars
+
+?>
