9166879715cff45db30dcf8a26262990adf54397
Binh Vinh Mai <bvmvw5@umkc.edu>
2016-11-29 14:09:30 -0600

    Started work on the search feature and added templates for use

--- script/cars.js
+++ script/cars.js
@@ -1,25 +1,26 @@
 $(document).ready(init);
 
-
 //Copied straight from search.hs in student_ajax, need to modify actually
 function init(){
-    $("#search_button").on("click",view_students);
-    $("#search").on("keydown",function(event){view_students_key(event);});
+    //If user clicks the magnifying glass
+    $("#find-car").on("click",view_cars());
+    //If user hits 'Enter' after a search
+    $("#find-car-input").on("keydown",function(event){view_cars_key(event);});
 }
 
-function view_students_key(event) {
+function view_cars_key(event) {
     if (event.keyCode == 13) //ENTER KEY
-        view_students();
+        view_cars();
 }
 
-function view_students() {
+function view_cars() {
     $.ajax({
         method: "POST",
         url: "search.php",
         dataType: "text", //return text data
         data: {search: $("#search").val()}, //send the value of the search box
         success: function (data) {
-            $("#students").html(data); //the returned result is the HTML of the students div
+            $("#returned_cars").html(data); //the returned result is the HTML of the students div
         }
     });   
 }
--- server/search.php
+++ server/search.php
@@ -1,6 +1,29 @@
 <?php
 
 require_once 'connection.php';
-$student = ""; //HTML for the cars
+$returned_cars = ""; //HTML for the cars
+
+if (isset($_POST['find-car-input']) && trim($_POST['find-car-input']) != "") {
+    $data = $_POST['find-car-input'];
+    
+    /* select all cars where any of the description is similar to the input */
+    $query = "SELECT * FROM car INNER JOIN carspecs ON car.CarSpecsID = carspecs.ID WHERE Make like '%$data%' OR Model like '%$data%' OR YearMade like '%$data%'";
+    $result = mysqli_query($connection,$query);
+
+    if (!$result)
+        die("Database access failed: " . mysqli_error($connection));
+
+    $row_count = mysqli_num_rows($result);
+
+    //change the below 
+    for ($j = 0; $j < $row_count; ++$j) {
+        $row = mysqli_fetch_array($result); //fetch the next row
+        $returned_cars.=display_student($row);
+    }
+    
+    echo $student;
+    
+    mysqli_close($connection);
+}
 
 ?>
